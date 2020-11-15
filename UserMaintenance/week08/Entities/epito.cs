7263377ff5e38e69744171fe336ab1652ee0a0df
Balázs Koczur <balazs.koczur@stud.uni-corvinus.hu>
2020-11-15 22:14:50 +0100

    2-3

---
+++ UserMaintenance/week08/Entities/epito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week08.Entities
+{
+    class epito
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
