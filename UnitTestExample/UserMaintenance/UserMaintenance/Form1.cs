f942c186231c5477878a84c81e2034a713434bef
Balázs Koczur <balazs.koczur@stud.uni-corvinus.hu>
2020-11-22 19:49:18 +0100

    2

---
+++ UnitTestExample/UserMaintenance/UserMaintenance/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserMaintenance
+{
+    public partial class Form1 : Form
+    {
+        BindingList<User> users = new BindingList<User>();
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
+        }
+    }
+}
